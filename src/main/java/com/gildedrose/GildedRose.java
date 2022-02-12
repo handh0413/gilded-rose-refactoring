db684e1a62912a46d5a685a9a00bc7b6ebd404b8
Donghee Han <handh0413@naver.com>
2022-02-12 19:52:59 +0900

    코드 리팩토링(Extract Method)
    [단축키]
    - Alt + Enter

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -13,19 +13,23 @@ class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             Item item = items[i];
-            GildedRoseItem gildedRoseItem;
-            if (item.name.equals(AGED_BRIE)) {
-                gildedRoseItem = new AgedBrieItem(item);
-            } else if (item.name.equals(BACKSTAGE_PASS)) {
-                gildedRoseItem = new BackstagePassItem(item);
-            } else if (item.name.equals(SULFURAS)) {
-                gildedRoseItem = new SulfurasItem(item);
-            } else {
-                gildedRoseItem = new NormalItem(item);
-            }
-            gildedRoseItem.updateQuality(item);
+            updateQuality(item);
             updateSellIn(item);
         }
+    }
+
+    private void updateQuality(Item item) {
+        GildedRoseItem gildedRoseItem;
+        if (item.name.equals(AGED_BRIE)) {
+            gildedRoseItem = new AgedBrieItem(item);
+        } else if (item.name.equals(BACKSTAGE_PASS)) {
+            gildedRoseItem = new BackstagePassItem(item);
+        } else if (item.name.equals(SULFURAS)) {
+            gildedRoseItem = new SulfurasItem(item);
+        } else {
+            gildedRoseItem = new NormalItem(item);
+        }
+        gildedRoseItem.updateQuality(item);
     }
 
     private void updateSellIn(Item item) {
