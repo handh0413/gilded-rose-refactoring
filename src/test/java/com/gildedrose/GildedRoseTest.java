8d5fba3bc9c3d06fbf2fa059df8e6314cefe259f
Donghee Han <handh0413@naver.com>
2022-02-12 18:44:39 +0900

    테스트 코드 추가

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 public class GildedRoseTest {
 
 	private static final String NONAME = "noname";
+	private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+	private static final String AGED_BRIE = "Aged Brie";
 
 	@Test
 	public void noname_sellin_0_quality_0() {
@@ -38,6 +40,34 @@ public class GildedRoseTest {
 		assertEquals(0, items[0].quality);
 	}
 
+	// Sulfuras는 전설의 아이템이다. 절대 팔지도 않고 품질이 떨어지지도 않는다.(판매 가능 날짜도 감소하지 않는다.)
+	@Test
+	public void sulfuras_sellin_0_quality_1() {
+		// 1. assign
+		Item[] items = new Item[] { new Item(SULFURAS, 0, 1) };
+		GildedRose gildedRose = new GildedRose(items);
 
+		// 2. action
+		gildedRose.updateQuality();
+
+		// 3. assert
+		assertEquals(0, items[0].sellIn);
+		assertEquals(1, items[0].quality);
+	}
+
+	// Aged Brie는 시간이 지날수록 품질이 증가한다. (판매 가능 날짜가 끝나면 +1 추가 증가한다.)
+	@Test
+	public void agedBrie_sellin_0_quality_0() {
+		// 1. assign
+		Item[] items = new Item[] { new Item(AGED_BRIE, 0, 0) };
+		GildedRose gildedRose = new GildedRose(items);
+
+		// 2. action
+		gildedRose.updateQuality();
+
+		// 3. assert
+		assertEquals(-1, items[0].sellIn);
+		assertEquals(2, items[0].quality);
+	}
 
 }
