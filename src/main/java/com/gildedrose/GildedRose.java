cab4b052ab817c9ece9c99699194afd4f4d5f278
Donghee Han <handh0413@naver.com>
2022-02-12 19:08:55 +0900

    코드 리팩토링(invert if condition)

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -48,19 +48,19 @@ class GildedRose {
             }
 
             if (item.sellIn < 0) {
-                if (!item.name.equals(AGED_BRIE)) {
-                    if (!item.name.equals(BACKSTAGE_PASS)) {
+                if (item.name.equals(AGED_BRIE)) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                } else {
+                    if (item.name.equals(BACKSTAGE_PASS)) {
+                        item.quality = item.quality - item.quality;
+                    } else {
                         if (item.quality > 0) {
                             if (!item.name.equals(SULFURAS)) {
                                 item.quality = item.quality - 1;
                             }
                         }
-                    } else {
-                        item.quality = item.quality - item.quality;
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
                     }
                 }
             }
