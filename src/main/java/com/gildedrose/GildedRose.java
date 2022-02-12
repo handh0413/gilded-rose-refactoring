4224665c9774762acb0ea14dedc6db7ecde77a91
Donghee Han <handh0413@naver.com>
2022-02-12 19:17:45 +0900

    코드 리팩토링(merge 'else if')

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -33,13 +33,11 @@ class GildedRose {
                         }
                     }
                 }
-            } else {
-                if (item.name.equals(SULFURAS)) {
+            } else if (item.name.equals(SULFURAS)) {
 
-                } else {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
+            } else {
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
                 }
             }
 
@@ -53,20 +51,16 @@ class GildedRose {
                         item.quality = item.quality + 1;
                     }
                 }
-            } else {
-                if (item.name.equals(BACKSTAGE_PASS)) {
-                    if (item.sellIn < 0) {
-                        item.quality = item.quality - item.quality;
-                    }
-                } else {
-                    if (item.name.equals(SULFURAS)) {
+            } else if (item.name.equals(BACKSTAGE_PASS)) {
+                if (item.sellIn < 0) {
+                    item.quality = item.quality - item.quality;
+                }
+            } else if (item.name.equals(SULFURAS)) {
 
-                    } else {
-                        if (item.sellIn < 0) {
-                            if (item.quality > 0) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
+            } else {
+                if (item.sellIn < 0) {
+                    if (item.quality > 0) {
+                        item.quality = item.quality - 1;
                     }
                 }
             }
