7fd367dbcce7e2fcdd92d605592b3b2e249331ab
Donghee Han <handh0413@naver.com>
2022-02-12 19:23:54 +0900

    코드 리팩토링(extract method)

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -14,52 +14,72 @@ class GildedRose {
         for (int i = 0; i < items.length; i++) {
             Item item = items[i];
             if (item.name.equals(AGED_BRIE)) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-
-                if (item.sellIn < 1) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
+                updateQualityForAgedBrie(item);
             } else if (item.name.equals(BACKSTAGE_PASS)) {
+                updateQualityForBackstagePasses(item);
+            } else if (item.name.equals(SULFURAS)) {
+                updateQualityForSulfuras(item);
+            } else {
+                updateQualityForNormalItem(item);
+            }
+
+            updateSellIn(item);
+        }
+    }
+
+    private void updateQualityForAgedBrie(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+
+        if (item.sellIn < 1) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+            }
+        }
+    }
+
+    private void updateQualityForBackstagePasses(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+
+            if (item.sellIn < 11) {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
-
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-
-                    if (item.sellIn < 1) {
-                        item.quality = item.quality - item.quality;
-                    }
-                }
-            } else if (item.name.equals(SULFURAS)) {
-
-            } else {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
-
-                if (item.sellIn < 1) {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
                 }
             }
 
-            if (!item.name.equals(SULFURAS)) {
-                item.sellIn = item.sellIn - 1;
+            if (item.sellIn < 6) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
             }
+
+            if (item.sellIn < 1) {
+                item.quality = item.quality - item.quality;
+            }
+        }
+    }
+
+    private void updateQualityForSulfuras(Item item) {
+
+    }
+
+    private void updateQualityForNormalItem(Item item) {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
+        }
+
+        if (item.sellIn < 1) {
+            if (item.quality > 0) {
+                item.quality = item.quality - 1;
+            }
+        }
+    }
+
+    private void updateSellIn(Item item) {
+        if (!item.name.equals(SULFURAS)) {
+            item.sellIn = item.sellIn - 1;
         }
     }
 }
