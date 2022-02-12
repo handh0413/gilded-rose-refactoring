b83607f33997d10a4f738d1979fa8298c3b6db92
Donghee Han <handh0413@naver.com>
2022-02-12 18:43:53 +0900

    테스트 코드 추가

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -23,4 +23,21 @@ public class GildedRoseTest {
 		assertEquals(0, items[0].quality);
 	}
 
+	// 재고를 업데이트할 때마다 판매 가능 일자(sellin)와 품질(quality)은 1씩 감소한다.
+	@Test
+	public void noname_sellin_0_quality_1() {
+		// 1. assign
+		Item[] items = new Item[] { new Item(NONAME, 0, 1) };
+		GildedRose gildedRose = new GildedRose(items);
+
+		// 2. action
+		gildedRose.updateQuality();
+
+		// 3. assert
+		assertEquals(-1, items[0].sellIn);
+		assertEquals(0, items[0].quality);
+	}
+
+
+
 }
